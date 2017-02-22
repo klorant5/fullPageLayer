9ba4c3d3fb8f53063732cb86adc410f735a05e05
klorant5 <klorant5@gmail.com>
2017-02-22 21:59:54 +0100

    commit

--- fullpage_layer.js
+++ fullpage_layer.js
@@ -1,31 +1,30 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
 
-    $("#fullpage_layer").fullpageLayer({
-        
-    });
-    
+    $("#fullpage_layer").fullpageLayer({});
+
 });
 
 
+(function ($) {
 
-(function( $ ) {
-    
     var ENABLED = 'fpl-enabled';
     var CONTENT_CLASS = 'fpl-content';
-    var SLIDE_NEXT ='fpl-slide-next';
-    var SLIDE_PREV ='fpl-slide-prev';
-    
- 
-    $.fn.fullpageLayer = function(options) {
- 
-        if($('html').hasClass(ENABLED)){ return; } //displayWarnings();
+    var SLIDE_NEXT = 'fpl-slide-next';
+    var SLIDE_PREV = 'fpl-slide-prev';
+
+
+    $.fn.fullpageLayer = function (options) {
+
+        if ($('html').hasClass(ENABLED)) {
+            return;
+        } //displayWarnings();
         var $pluginContent = $(this);
         var $pageHeight = 0;
         var $pageWidth = 0;
         var $currentLayerIndex = 1;
         var $maxLayerCount = 0;
 
-        
+
         options = $.extend({
 //            menu: false,
 //            afterReBuild: null,
@@ -35,11 +34,10 @@ jQuery(document).ready(function($){
         init();
 
         function getActiveLeftMarginOfSlideShow() {
-            return $(".layer.active .slideshow").css("margin-left").replace("px","")*1;
+            return $(".layer.active .slideshow").css("margin-left").replace("px", "") * 1;
         }
 
-        function init()
-        {
+        function init() {
             console.log("--init--");
             $pluginContent.addClass(CONTENT_CLASS);
             $('html').addClass(ENABLED);
@@ -48,13 +46,13 @@ jQuery(document).ready(function($){
             $pluginContent.append($("<a>").addClass(SLIDE_NEXT));
             $pluginContent.append($("<a>").addClass(SLIDE_PREV));
             $pluginContent.css({
-                height : '100%',
-                position : 'relative'
+                height: '100%',
+                position: 'relative'
             });
             $pageHeight = $pluginContent.height();
 
             $(".layer").css({
-                height:'100%'
+                height: '100%'
             });
             $(".layer:first").addClass("active");
             $(".layer").not(":first").addClass("fpl-next").css({
@@ -63,29 +61,29 @@ jQuery(document).ready(function($){
 
             $pageWidth = $("body").width();
 
-            $(".layer").each(function(i){
-                $(this).css("z-index", i+1);
+            $(".layer").each(function (i) {
+                $(this).css("z-index", i + 1);
 
                 $maxLayerCount += 1;
             });
 
             $(".layer .slide").css({
-                height : '100%',
-                width : $pageWidth,
-                float : 'left'
+                height: '100%',
+                width: $pageWidth,
+                float: 'left'
             });
 
-            $(".slideshow").css("height", $pageHeight+"px");    //???
+            $(".slideshow").css("height", $pageHeight + "px");    //???
 
             $(".layer .slideshow").each(function (i) {
                 var $slideshowWidth = $(this).children(".slide").length * $pageWidth;
-                console.log('slideshowWidth:',$slideshowWidth);
+                console.log('slideshowWidth:', $slideshowWidth);
                 $(this).width($slideshowWidth);
             });
 
             //scroll
-            $('body').on( 'DOMMouseScroll mousewheel', function ( event ) {
-                if( event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0 ) { //alternative options for wheelData: wheelDeltaX & wheelDeltaY
+            $('body').on('DOMMouseScroll mousewheel', function (event) {
+                if (event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0) { //alternative options for wheelData: wheelDeltaX & wheelDeltaY
                     //scroll down
                     var t = setTimeout(function () {
                         next();
@@ -104,21 +102,21 @@ jQuery(document).ready(function($){
             });
 
 
-            $("."+SLIDE_NEXT).on("click",function(){
+            $("." + SLIDE_NEXT).on("click", function () {
                 console.log("SLIDE NEXT CLICK");
 
                 $(".layer.active .slideshow").animate({
-                    'margin-left' : (getActiveLeftMarginOfSlideShow() - $pageWidth)+'px'
+                    'margin-left': (getActiveLeftMarginOfSlideShow() - $pageWidth) + 'px'
                 }, 700, 'swing', function () {
                     //complete
 
                 });
             });
-            $("."+SLIDE_PREV).on("click",function(){
+            $("." + SLIDE_PREV).on("click", function () {
                 console.log("SLIDE PREV CLICK");
 
                 $(".layer.active .slideshow").animate({
-                    'margin-left' : (getActiveLeftMarginOfSlideShow() + $pageWidth)+'px'
+                    'margin-left': (getActiveLeftMarginOfSlideShow() + $pageWidth) + 'px'
                 }, 700, 'swing', function () {
                     //complete
 
@@ -131,23 +129,24 @@ jQuery(document).ready(function($){
 
         function toggleSlideShowArrows() {
 
-            if($(".layer.active .slide").length){
-                $("."+SLIDE_NEXT).show();
-                $("."+SLIDE_PREV).show();
-            }else{
-                $("."+SLIDE_NEXT).hide();
-                $("."+SLIDE_PREV).hide();
+            if ($(".layer.active .slide").length) {
+                $("." + SLIDE_NEXT).show();
+                $("." + SLIDE_PREV).show();
+            } else {
+                $("." + SLIDE_NEXT).hide();
+                $("." + SLIDE_PREV).hide();
             }
         }
+
         var $called = false;
 
-        function next(){
-            if(!$called) {
+        function next() {
+            if (!$called) {
                 $called = !$called;
                 console.log("next");
 
                 var nextLayerIndex = $currentLayerIndex + 1;
-                if(nextLayerIndex <= $maxLayerCount){
+                if (nextLayerIndex <= $maxLayerCount) {
                     console.log("next - CALLED");
 
                     var nextLayer = $(".layer:nth-child(" + nextLayerIndex + ")");
@@ -167,14 +166,15 @@ jQuery(document).ready(function($){
                         $called = !$called;
                     });
 
-                }else{
+                } else {
                     toggleSlideShowArrows();
                     $called = !$called;
                 }
             }
         }
-        function previous(){
-            if(!$called) {
+
+        function previous() {
+            if (!$called) {
                 $called = !$called;
                 console.log("previous");
 
@@ -196,15 +196,15 @@ jQuery(document).ready(function($){
                         toggleSlideShowArrows();
                         $called = !$called;
                     });
-                }else{
+                } else {
                     toggleSlideShowArrows();
                     $called = !$called;
                 }
 
             }
         }
-        
- 
+
+
     };
- 
-}( jQuery ));
+
+}(jQuery));
